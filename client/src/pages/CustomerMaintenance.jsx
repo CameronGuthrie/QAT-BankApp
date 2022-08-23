d54a23ade41ab132634c6ca858c7941e1a530379
tjayling <tjayling@yahoo.co.uk>
2022-08-23 14:59:56 +0100

    Added files and routes and populated main menu with buttons

---
+++ client/src/pages/CustomerMaintenance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerMaintenance = () => {
+  return (
+    <div>CustomerMaintenance</div>
+  )
+}
+
+export default CustomerMaintenance
---
+++ client/src/pages/MainMenu.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MainMenu = () => {
+
+  return (
+    <div>
+      MainMenu
+      <div id="button-containers">
+        <button>Log In</button>
+        <button>Customer Search</button>
+        <button>Create New Customer</button>
+        <button>Open New Account</button>
+        <button>New Transaction</button>
+        <button>Transfer</button>
+      </div>
+    </div>
+  );
+};
+
+export default MainMenu;
