b91963ad5cafcf2ecbc136363dba33e4c9d9379f
tjayling <tjayling@yahoo.co.uk>
2022-08-23 17:33:22 +0100

    Added username and password input fields with checks on the inputs

--- client/src/pages/MainMenu.jsx
+++ client/src/pages/MainMenu.jsx
@@ -1,12 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Popup from "../components/Popup";
-import "../components/css/popup.css";
+import Error from "../components/Error";
 
 const MainMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [usernameError, setUsernameError] = useState(<></>);
+  const [passwordError, setPasswordError] = useState(<></>);
 
   const togglePopup = () => {
     setIsOpen(!isOpen);
+  };
+
+  const checkUsername = () => {
+    let input = document.querySelector("#username-input");
+    setUsernameError(
+      input.value.length > 20 ? (
+        <Error message="Username is too long" />
+      ) : input.value.length == 0 ? (
+        <Error message="Enter a username" />
+      ) : new RegExp(/^[a-zA-Z0-9]+$/).test(input.value) ? (
+        <></>
+      ) : (
+        <Error message="Invalid characters in username" />
+      )
+    );
+  };
+
+  const checkPassword = () => {
+    let input = document.querySelector("#password-input");
+    setPasswordError(
+      new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/).test(input.value) ? (
+        <></>
+      ) : (
+        <Error message="Password must be minimum eight and maximum 20 characters, at least one uppercase letter, one lowercase letter, one number and one special character: " />
+      )
+    );
   };
 
   return (
@@ -16,7 +44,11 @@ const MainMenu = () => {
         <Popup
           content={
             <>
-              <p>Login Popup</p>
+              <h2 id="popup-title">Login</h2>
+              <input type="text" id="username-input" placeholder="Username..." onBlur={checkUsername} />
+              {usernameError}
+              <input type="password" id="password-input" placeholder="Password..." onBlur={checkPassword} />
+              {passwordError}
             </>
           }
           handleClose={togglePopup}
