b4db1e846151aa7b3fc4566019d4e08f8ab779e3
tjayling <tjayling@yahoo.co.uk>
2022-08-23 15:01:40 +0100

    added unpopulated but functioning login popup on main menu

--- client/src/pages/MainMenu.jsx
+++ client/src/pages/MainMenu.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Popup from "../components/Popup";
+import "../components/css/popup.css";
 
 const MainMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
       MainMenu
+      {isOpen && (
+        <Popup
+          content={
+            <>
+              <p>Login Popup</p>
+            </>
+          }
+          handleClose={togglePopup}
+        />
+      )}
       <div id="button-containers">
-        <button>Log In</button>
+        <button onClick={togglePopup}>Log In</button>
         <button>Customer Search</button>
         <button>Create New Customer</button>
         <button>Open New Account</button>
